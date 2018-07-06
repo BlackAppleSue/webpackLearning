9cc33eb5b319a4f7809999695ffd13f89ede51ef
BlackAppleSue <gn00644368@livemail.tw>
2018-07-06 17:32:35 +0800

    add style and img

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './style.css';
+import apply_car from './apply_car.png';
+
+
+console.log(apply_car)
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,33 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry:{
-        app:['./src/index.js','./src/a.js','./src/b.js']
+    entry: {
+        app: ['./src/index.js', './src/a.js', './src/b.js']
     },
-    output:{
-        filename:'[name].js',
-        path:path.resolve(__dirname,'dist')
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'ab'
+        })
+    ],
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },{
+                test:/\.(png|svg|jpg|gif)$/,
+                use:[
+                    'file-loader'
+                ]
+            }
+        ]
     }
 }
